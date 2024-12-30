08b3574484779255ba9f5d5ee84a921a66df5147
andecodelima <andrelimag89@gmail.com>
2024-12-30 16:32:48 -0300

    feat: add de novos endpoints

--- sistema-gestao/src/main/java/net/sys/gest/controller/AcessoController.java
+++ sistema-gestao/src/main/java/net/sys/gest/controller/AcessoController.java
@@ -1,14 +1,17 @@
 package net.sys.gest.controller;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import net.sys.gest.model.Acesso;
 import net.sys.gest.repository.AcessoRepository;
@@ -30,26 +33,93 @@ public class AcessoController {
 		return "/index";
 	}
 	
+	
+	
+	
 	 
 	@PostMapping(value="/saveAcesso")
-	public ResponseEntity<Acesso> saveAcesso(@RequestBody Acesso acesso){
+	public ResponseEntity<Acesso> saveAcesso(@Validated  @RequestBody Acesso acesso){
 		
-		Acesso acessoSalvo = acessoServiceInterface.saveAcesso(acesso);
-		return ResponseEntity.status(HttpStatus.CREATED).body(acessoSalvo);
+//		Acesso acessoSalvo = acessoServiceInterface.saveAcesso(acesso);
+//		return ResponseEntity.status(HttpStatus.CREATED).body(acessoSalvo);
+		
+		try {
+		    System.out.println("Payload recebido: " + acesso);
+			Acesso acessoSalvo = acessoServiceInterface.saveAcesso(acesso);
+			return ResponseEntity.status(HttpStatus.CREATED).body(acessoSalvo);
+		}catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 		
 	}
 	
 	
-	@DeleteMapping(value="/deleteAcesso")
-	public ResponseEntity<Acesso>deleteAcesso(@RequestBody Acesso acesso){
-		acessoRepository.deleteById(acesso.getId());
+	@DeleteMapping(value="/deleteAcesso/{id}")
+	public ResponseEntity<String> deleteAcesso(@PathVariable Long id){
+				
+//		try {
+//			acessoRepository.deleteById(id);
+//			return ResponseEntity.status(HttpStatus.OK).body("Aesso removido com sucesso");
+//			
+//		}catch (Exception e) {
+//			 e.printStackTrace();
+//			 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao remover acesso");
+//		}
 		
-		return new ResponseEntity("Acesso removido", HttpStatus.OK);
+		try {
+			boolean registroRemovido = acessoServiceInterface.deleteAcesso(id);
+			if(registroRemovido) {
+				return ResponseEntity.status(HttpStatus.OK).body("Acesso removido com sucesso");
+			
+			}else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Acesso não encontrado");
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao remover Acesso");
+		}
+		
 	}
 	
 	
 	
+//	// 1- FORMA DE FAZER - RETORNO EXPLICITO DO OBJETO
+//	@GetMapping(value="/findAcessoById/{id}")
+//	public ResponseEntity<Acesso> findAcessoById(@PathVariable Long id){
+//		
+//		
+////		try {
+////			Acesso acesso = acessoServiceInterface.findAcessoById(id);
+////			return ResponseEntity.status(HttpStatus.OK).body(acesso);
+////			
+////		}catch (Exception e) {
+////			e.printStackTrace();
+////			 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+////		}
+//		
+//	}
 	
+
+
+	// 2- FORMA RETORNO GENÉRICO 
+	@GetMapping(value="/findAcessoById/{id}")
+	public ResponseEntity<?> findAcessoById(@PathVariable Long id){
+		
+		try {
+			Acesso acesso  = acessoServiceInterface.findAcessoById(id);
+			return ResponseEntity.status(HttpStatus.OK).body(acesso);
+			
+		} catch (NoSuchElementException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Acesso não encontrado para o Id: " + id);
+		
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro encontrado ao buscar o acesso: " + e);
+		}
+	
+	
+	}
 	
 	
 		
--- sistema-gestao/src/main/java/net/sys/gest/service/AcessoServiceImplementation.java
+++ sistema-gestao/src/main/java/net/sys/gest/service/AcessoServiceImplementation.java
@@ -1,12 +1,20 @@
 package net.sys.gest.service;
 
+import java.util.Collection;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import net.sys.gest.model.Acesso;
 import net.sys.gest.repository.AcessoRepository;
 
 @Service
+@Transactional
 public class AcessoServiceImplementation implements AcessoServiceInterface {
 
 	@Autowired
@@ -19,5 +27,60 @@ public class AcessoServiceImplementation implements AcessoServiceInterface {
 		return acessoRepository.save(acesso);
 	}
 
+
+	@Override
+	public Boolean deleteAcesso(Long id) {
+		 
+		Optional<Acesso> temp = acessoRepository.findById(id);
+		if(temp.isPresent()) {
+			acessoRepository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+	
+
+	@Override
+	public Acesso findAcessoById(Long id) {
+		Optional<Acesso> temp = acessoRepository.findById(id);
+		if(temp.isEmpty()) {
+			throw new NoSuchElementException("Acesso não encontrado" + id);
+		}
+		
+		return temp.get();
+	}
+
+
+	@Override
+	public Acesso editAcesso(Acesso acesso) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Collection<Acesso> getAllAcesso() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	// Teste de Inserir
+/*	@EventListener(ApplicationReadyEvent.class)
+	 public void testarSalvarAcesso() {
+		 Acesso acesso = new Acesso();
+		 acesso.setDescricao("Teste Acesso");
+		 
+		 try {
+			 Acesso acessoSalvo = this.saveAcesso(acesso);
+			 System.out.println("Registro salvo com sucesso: "  + acessoSalvo);
+		 }catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Erro ao salvar registro");
+		}
+	 
+	 }
+*/
 	
 }
