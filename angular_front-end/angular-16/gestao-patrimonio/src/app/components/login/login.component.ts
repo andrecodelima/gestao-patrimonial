439d749fa5dd257f30a06beb7eee4ec9d22f24e8
andecodelima <andrelimag89@gmail.com>
2025-01-15 15:03:02 -0300

    fix: formulario login feito

--- angular_front-end/angular-16/gestao-patrimonio/src/app/components/login/login.component.ts
+++ angular_front-end/angular-16/gestao-patrimonio/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private fb: FormBuilder){
+
+  }
+
+  // Pegando os dados do formulario
+  loginForm = this.fb.group({
+    id:[],
+    login:[null, Validators.required],
+    senha:[null, Validators.required]
+  })
+
+
+  // Transformando em objeto
+  loginObjeto(): Usuario{
+    return{
+      login: this.loginForm.get('login')?.value!,
+      senha: this.loginForm.get('senha')?.value!
+    }
+  }
+
+  fazerLogin(){
+    const usuario = this.loginObjeto();
+    console.info('dados de login ->' + usuario.login);
+    console.info('dados de login ->' + usuario.senha);
+
+  }
 }
--- angular_front-end/angular-16/gestao-patrimonio/src/app/services/cadastro-acesso.service.ts
+++ angular_front-end/angular-16/gestao-patrimonio/src/app/services/cadastro-acesso.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../model/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CadastroAcessoService {
 
-  constructor() { }
+  private urlApiLocal = environment.urlApiLocal + 'login';
+  constructor(private http: HttpClient) { 
+
+  }
+
+  logar(usuario: Usuario){
+
+    return this.http.post<String>(this.urlApiLocal, usuario);
+  }
+
 }
